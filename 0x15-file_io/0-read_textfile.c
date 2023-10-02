1605b6ec273eea4fc5e38d10ebea4c15e4772abc
Katbiz <ramothamok77@gmail.com>
2023-10-02 08:04:53 -0700

    File I/O project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- this  funtion reads the text
+ * file print to the STDOUT.
+ *
+ * @filename: this is the text file being read
+ * @letters: This is the number of letters to be read
+ * Return: v- which is the actual number of bytes read and printed
+ *        and 0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t sc;
+	ssize_t u;
+	ssize_t v;
+
+	sc = open(filename, O_RDONLY);
+	if (sc == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	v = read(sc, buf, letters);
+	u = write(STDOUT_FILENO, buf, v);
+
+	free(buf);
+	close(sc);
+	return (u);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates the file.
+ *
+ * @filename: it'ss a pointer to the name of
+ * the file to be created.
+ * @text_content: it is a pointer to a string to write to the file.
+ *
+ * Return: If the function fails, it will return -1.
+ *         Otherwise otherwise it returns 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int sc, u, leng = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (leng = 0; text_content[leng];)
+			leng++;
+	}
+
+	sc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	u = write(sc, text_content, leng);
+
+	if (sc == -1 || u == -1)
+		return (-1);
+
+	close(sc);
+
+	return (1);
+}
